8d7917305a7c3f57c6f7a38124a5a8fac9dd2e7b
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-11-10 20:35:49 -0800

    creating user if user does not exist in database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,9 +13,32 @@ const config =
     appId: "1:973158806735:web:c6459a3ac8afb5e66c33ba",
     measurementId: "G-HYJL74W8CG"
 };
-
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
